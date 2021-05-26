c0e0f112c0392c01d609364971d5d39e6232d831
ShellyNR <73890534+ShellyNR@users.noreply.github.com>
2021-05-26 11:10:59 +0300

    Update expServer.js

--- controller/expServer.js
+++ controller/expServer.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const fileUpload = require ('express-fileupload')
 const app = express()
-//const api = require('/home/barsh/metkadem2/A2/model\\build\\Release\\model')
-
+const api = require('C:\\Users\\Shelly Nahir\\Desktop\\uni\\ass5\\model\\build\\Release\\model')
+const path = require('path')
+const fetch = require("node-fetch");
 
 app.use(express.static('../view'))
 app.use(fileUpload())
@@ -13,39 +14,44 @@ app.use(express.urlencoded({
 }))
 
 app.get('/', (req, res)=> {
-    res.sendFile('/home/barsh/metkadem2/A2/view/view.html')
+    res.sendFile('C:\\Users\\Shelly Nahir\\Desktop\\uni\\ass5\\view\\view.html')
 })
 
-app.get('/anomaly', (req, res)=> {
-        jsonAnomaly = {
-                        "1": {
-                            "f1": "alt",
-                            "f2": "speed",
-                            "lineNum": "12"
-                        },
-                        "2": {
-                            "f1": "bar",
-                            "f2": "shelly",
-                            "lineNum": "20"
-                        },
-                        "3": {
-                            "f1": "roi",
-                            "f2": "nir",
-                            "lineNum": "55"
-                        },
-                        "4": {
-                            "f1": "Tel aviv",
-                            "f2": "Ramat gan",
-                            "lineNum": "2"
-                        },
 
-                    }
-    res.write(JSON.stringify(jsonAnomaly))
+
+/**
+app.post('/upload',(req, res) => {
+    //console.log(req.files.TrainFile)
+    //res.write(req.files.TrainFile.toString())
+    res.write(api.calc(path.dirname(req.files.TrainFile.name.toString()), req.files.TestFile.name, 1).msg)
+    /** 
+    res.write('Processing')
+    if (req.files.TrainFile != null && req.files.TestFile != null) {
+        var train = req.files.TrainFile
+        var test = req.files.TestFile
+        // train.data.toString(), test.data.toString()
+    }
+    
     res.end()
+
 })
+**/
+
+function jsonToTable(obj) {
+    var ret = "";
+    for (var o in obj) {
+        var data = obj[o];
+        if (typeof data !== 'object') {
+            ret += "<li>" + o + " : " + data + "</li>";
+        } else {
+            ret += "<li>" + o + " : " + jsonToTable(data) + "</li>";
+        }
+    }
+    return "<ul>" + ret + "</ul>";
+}
 
 app.post('/upload',(req, res) => {
-    res.write('Processing')
+    //res.write('Processing')
 
     if ("TrainFile" in req.files && "TestFile" in req.files) {
         var test_data = req.files.TestFile.data
@@ -53,20 +59,52 @@ app.post('/upload',(req, res) => {
         var model = req.body.models
         var fs = require('fs')
 
-        fs.writeFile("temp_files/test.csv", test_data, function(err) {
+        fs.writeFile("../files/test.csv", test_data, function(err) {
             if(err) {
                 return console.log(err)
             }
         }); 
-        fs.writeFile("temp_files/train.csv", train_data, function(err) {
+        fs.writeFile("../files/train.csv", train_data, function(err) {
             if(err) {
                 return console.log(err)
             }
         }); 
         
         console.log(model)
+
+        jsonAnomaly = {
+            "1": {
+                "f1": "alt",
+                "f2": "speed",
+                "lineNum": "12"
+            },
+            "2": {
+                "f1": "bar",
+                "f2": "shelly",
+                "lineNum": "20"
+            },
+            "3": {
+                "f1": "roi",
+                "f2": "nir",
+                "lineNum": "55"
+            },
+            "4": {
+                "f1": "Tel aviv",
+                "f2": "Ramat gan",
+                "lineNum": "2"
+            },
+
+        }
+
+        const anomalies = require('../files/anomaly-report.json');
+
+        for (var o in anomalies) {
+            res.write(o+". "+anomalies[o].cor_feat+": "+anomalies[o].time+'\n')            
+        }
+
     }
     res.end()
 
 })
+
 app.listen(8080)
